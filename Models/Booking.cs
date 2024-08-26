0066487b5ee4d4b812a972a9d5133adb06ecfaeb
Divu5643 <divyanshwpbackup@gmail.com>
2024-08-26 20:11:43 +0530

    Add files via upload

---
+++ Models/Booking.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieBookingApp.Models
+{
+    public class Booking
+    {
+        public int BookingId { get; set; }
+
+        public int ShowTimeID { get; set; }
+        public int NumberOfSeats { get; set; } = 100;
+
+        public string UserName { get; set; }
+
+        public string UserEmail { get; set; }
+    }
+}
